f0d0ab67b43ddeff9fe25b3b7937607a8b9d6ba0
ylmngo <aamerasim45@gmail.com>
2024-05-13 13:31:57 +0530

    Begin Rewrite

--- migrations/000003_create_files_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS files (
-    file_id bytea PRIMARY KEY, 
-    user_id bigint REFERENCES users ON DELETE CASCADE,  
-    filename text NOT NULL, 
-    metadata text NOT NULL,  
-    uploaded_at timestamp(0) with time zone NOT NULL DEFAULT NOW()  
-);                                                                      
